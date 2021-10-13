a2cc3c798af7053d91884860170e0d25667673c8
tatarints <tvlt2512@gmail.com>
2021-10-13 23:11:08 +0500

    Изменила методы для теста

--- lesson_3/Program.cs
+++ lesson_3/Program.cs
@@ -33,56 +33,11 @@ namespace lesson_3
         public float Y { get; set; }
     }
 
-
-    public class BechmarkClass
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class Bencmark
     {
-        
-        public static void RandomPoints()
-        {
-            Random rnd = new Random();
-            int x = rnd.Next(1, 100);
-            int y = rnd.Next(1, 100);
-
-
-            PointClass pointClass1 = new PointClass();
-            PointStruct pointStruct1 = new PointStruct();
-            PointStructDouble pointStructDouble1 = new PointStructDouble();
-            PointStructWithoutSquare pointStructWithoutSquare1 = new PointStructWithoutSquare();
-
-            pointClass1.X = x;
-            pointStruct1.X = pointClass1.X;
-            pointStructDouble1.X = pointStructDouble1.X;
-            pointStructWithoutSquare1.X = pointClass1.X;
-
-            pointClass1.Y = y;
-            pointStruct1.Y = pointClass1.Y;
-            pointStructDouble1.Y = pointStructDouble1.Y;
-            pointStructWithoutSquare1.Y = pointClass1.Y;
-
-            PointClass pointClass2 = new PointClass();
-            PointStruct pointStruct2 = new PointStruct();
-            PointStructDouble pointStructDouble2 = new PointStructDouble();
-            PointStructWithoutSquare pointStructWithoutSquare2 = new PointStructWithoutSquare();
-
-            pointClass2.X = x;
-            pointStruct2.X = pointClass1.X;
-            pointStructDouble2.X = pointStructDouble1.X;
-            pointStructWithoutSquare2.X = pointClass1.X;
-
-            pointClass2.Y = y;
-            pointStruct2.Y = pointClass1.Y;
-            pointStructDouble2.Y = pointStructDouble1.Y;
-            pointStructWithoutSquare2.Y = pointClass1.Y;
-
-
-            PointDistanceC(pointClass1, pointClass2);
-            PointDistanceS(pointStruct1, pointStruct2);
-            PointDistanceD(pointStructDouble1, pointStructDouble2);
-            PointDistanceWS(pointStructWithoutSquare1, pointStructWithoutSquare2);
-            Console.WriteLine();
-        }
-
-        [Benchmark]
+       
         static void PointDistanceC(PointClass point1, PointClass point2)
         {
             float x = point1.X - point2.X;
@@ -91,7 +46,6 @@ namespace lesson_3
             MathF.Sqrt((x * x) + (y * y));
         }
 
-        [Benchmark]
         static void PointDistanceS(PointStruct point1, PointStruct point2)
         {
             float x = point1.X - point2.X;
@@ -100,7 +54,6 @@ namespace lesson_3
             MathF.Sqrt((x * x) + (y * y));
         }
 
-        [Benchmark]
         static void PointDistanceD(PointStructDouble point1, PointStructDouble point2)
         {
             double x = point1.X - point2.X;
@@ -109,7 +62,6 @@ namespace lesson_3
             Math.Sqrt((x * x) + (y * y));
         }
 
-        [Benchmark]
         static void PointDistanceWS(PointStructWithoutSquare point1, PointStructWithoutSquare point2)
         {
             float x = point1.X - point2.X;
@@ -117,12 +69,36 @@ namespace lesson_3
 
             MathF.Sqrt((x * x) + (y * y));
         }
+    
+    
+        [Benchmark]
+        public void A()
+        {
+
+            PointDistanceC(new PointClass() { X = 0, Y = 0 }, new PointClass() { X = 110, Y = 110 });
+            
+        }
+        [Benchmark]
+        public void B()
+        {
+            PointDistanceS(new PointStruct() { X = 0, Y = 0 }, new PointStruct() { X = 110, Y = 110 });
+        }
+        [Benchmark]
+        public void C()
+        {
+            PointDistanceD(new PointStructDouble() { X = 0, Y = 0 }, new PointStructDouble() { X = 110, Y = 110 });
+        }
+        [Benchmark]
+        public void D()
+        {
+            PointDistanceWS(new PointStructWithoutSquare() { X = 0, Y = 0 }, new PointStructWithoutSquare() { X = 110, Y = 110 });
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            BenchmarkRunner.Run<Bencmark>();
         }
     }
 }
